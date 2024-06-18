307bbeb7e74265cdb23162c9ff877871dd98f1ef
Ahimsa Jenar Bramsaifstyo Kusuma <tio.rmx@gmail.com>
2024-06-18 13:19:27 +0700

    Trying class,method and property

---
+++ Belajar/Mobil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Materi
+{
+    public class Mobil
+    {
+        public string merk;
+        public string warna;
+        public int jumlahBan;
+
+        public void Maju()
+        {
+            Console.WriteLine("Mobil " + this.merk + " Jalan Maju");
+        }
+    }
+}
---
+++ Belajar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Materi;
+
+public class Program
+{
+	static void Main(string[]args)
+	{
+		Mobil mobil1 = new Mobil();
+		mobil1.merk = "Toyota";
+		mobil1.warna = "black";
+		mobil1.jumlahBan = 4;
+		mobil1.Maju();
+
+		Mobil mobil2 = new Mobil();
+		mobil2.merk = "Suzuki";
+		mobil2.warna = "white";
+
+		Console.ReadLine();
+	}
+}
